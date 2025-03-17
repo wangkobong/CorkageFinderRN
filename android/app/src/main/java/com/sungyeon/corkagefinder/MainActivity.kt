9c4cf2fec36060d7fff11974f53eeec2e97457e6
sungyeonkim <wangkobong@gmail.com>
2025-03-17 11:40:31 +0900

    feat - 구글로그인추가 2

--- android/app/src/main/java/com/sungyeon/corkagefinder/MainActivity.kt
+++ android/app/src/main/java/com/sungyeon/corkagefinder/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.wangkobong.corkagefinder
+package com.sungyeon.corkagefinder
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/sungyeon/corkagefinder/MainApplication.kt
+++ android/app/src/main/java/com/sungyeon/corkagefinder/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.wangkobong.corkagefinder
+package com.sungyeon.corkagefinder
 
 import android.app.Application
 import android.content.res.Configuration
